ab9966a0846b932704b54a717161985ede37d96f
theseeduse <qwert304411@naver.com>
2025-02-09 19:39:09 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+/* 테테테테토테토 테테테테토리스
+
+Copyright 2025 적기 귀찮으니 알아서 기억하쇼 ㅇㅇ*/
+
+const express = require('express');
+const pg = require('pg');
+require('dotenv').config;
+
+const host = process.env.HOST;
+const port = process.env.PORT;
