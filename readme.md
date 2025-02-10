47ef45c8e0ea7b341bf658cdcf9b325fd4739844
theseeduse <qwert304411@naver.com>
2025-02-11 00:23:34 +0900

    ㅇㅇ

--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 # Copyright 2025 KasaneTeto All right Reserved.
 ## 상세
 쓰지마쇼
+## 개발 방향
+ * the seed 모방
+  * the seed 보완
+ * 자유롭게 활용 가능한 엔진
+ * 생 html css js만 사용하기(시도)
--- server.js
+++ server.js
@@ -4,7 +4,10 @@ Copyright 2025 적기 귀찮으니 알아서 기억하쇼 ㅇㅇ*/
 
 import express from 'express';
 import pg from 'pg';
+const app = express();
+
 require('dotenv').config;
 
 const host = process.env.HOST;
 const port = process.env.PORT;
+const dbtype = process.env.DB_TYPE;
