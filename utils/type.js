a38208215a71fe39eb83e9890f890e9ce2f16e88
theseeduse <qwert304411@naver.com>
2025-02-11 00:25:30 +0900

    type 몇 개 얹기 (나머지는 귀찮아서 내일 추가함)

--- utils/type.js
+++ utils/type.js
@@ -2,9 +2,17 @@ export const types = {
     UserType: {
         "delete": 0,
         "ip": 1,
-        "author" 2
+        "author": 2
     },
     ACLType: {
-        "perm": 0
+        "perm": 0,
+        "user": 1,
+        "geoip": 2,
+        "aclgroup": 3
+    },
+    ACLUseType: {
+        "allow": 0,
+        "deny": 1,
+        "gotons": 2,
     }
 };
