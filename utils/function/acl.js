d01a631a061566ab349a0799872e3b4ff306693a
KasaneTeto <qwert304411@naver.com>
2025-02-11 09:28:53 +0900

    acl 가져오기

--- utils/function/acl.js
+++ utils/function/acl.js
@@ -1,1 +1,25 @@
-import { AclType, AclUseType, AclPermType } from '../type.js';
+import { ACLType, ACLExecuteType, ACLPermType } from '../type.js';
+
+function getACLType(ACLType) {
+  let ACLIndex = '';
+  
+  if(ACLType === 1) {
+    ACLIndex = 'user';
+  } else if(ACLType === 2) {
+    ACLIndex = 'geoip';
+  } else if(ACLType === 3) {
+    ACLIndex = 'aclgroup';
+  } else if(ACLType === 4) {
+    ACLIndex = 'ip';
+  } else ACLIndex = null;
+  
+  return ACLIndex;
+}
+
+function getACLExecuteType(ACLIndex) {
+  if(ACLIndex === null) return getACLPerm(ACLExecuteType);
+}
+
+function getACLPerm(ACLExecuteType) {
+  // 귀찮아
+}
