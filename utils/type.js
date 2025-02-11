7e1a2e94807f8e70d6a0f2b10f958c1f2e7f5ed7
KasaneTeto <qwert304411@naver.com>
2025-02-11 09:17:41 +0900

    acl 관련 속성 수정

--- utils/type.js
+++ utils/type.js
@@ -8,11 +8,23 @@ export const types = {
         "perm": 0,
         "user": 1,
         "geoip": 2,
-        "aclgroup": 3
+        "aclgroup": 3,
+        "ip": 4
     },
-    ACLUseType: {
+    ACLExecuteType: {
         "allow": 0,
         "deny": 1,
         "gotons": 2,
+    },
+    ACLPermType: {
+        "any": 0,
+        "author": 1,
+        "signup_15day_later": 2,
+        "docname_same_username": 3,
+        "auto_verfied": 4,
+        "document_contributor": 5,
+        "contributor": 6,
+        "admin": 7,
+        "ip": 8
     }
 };
