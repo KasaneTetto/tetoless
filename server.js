f5669b203c346ddb7be5087a57d5485f5fe1030b
theseeduse <qwert304411@naver.com>
2025-02-11 17:22:09 +0900

    아 몰라 처음부터 다시 만들어

--- server.js
+++ server.js
@@ -2,12 +2,20 @@
 
 Copyright 2025 적기 귀찮으니 알아서 기억하쇼 ㅇㅇ*/
 
+// import acl from './utils/function/acl';
 import express from 'express';
 import pg from 'pg';
-const app = express();
 
 require('dotenv').config;
 
 const host = process.env.HOST;
 const port = process.env.PORT;
 const dbtype = process.env.DB_TYPE;
+const app = express();
+
+if(dbtype === 'pg') {
+    console.log('현재 postgresql은 지원하지 않습니다. DB type를 sqlite로 변경한 뒤 초기화하고 다시 시작해주세요.');
+    process.exit(1);
+}
+
+app.listen()
