b8bcd75574e365563705ed09d6fd0338b5ce9b4d
theseeduse <qwert304411@naver.com>
2025-02-11 17:15:28 +0900

    이게 뭐하는 코드인지 난 하나도 모르겠다

--- utils/function/acl.js
+++ utils/function/acl.js
@@ -1,31 +1,41 @@
-import { ACLType, ACLExecuteType, ACLPermType } from '../type.js';
+// 난 내가 무슨 코드를 싸는지 전혀 모르겠다
 
-function getACLType(aclType) {
-  let aclIndex = null;
+function getACLtype(x) {
+  let ACLIndex = null;
   
-  if (aclType === 1) {
+  if (x === 1) {
     ACLIndex = 'user';
-  } else if (aclType === 2) {
+  } else if (x === 2) {
     ACLIndex = 'geoip';
-  } else if (aclType === 3) {
+  } else if (x === 3) {
     ACLIndex = 'aclgroup';
-  } else if (ACLType === 4) {
+  } else if (x === 4) {
     ACLIndex = 'ip';
   }
   
   return ACLIndex;
 }
 
-function getACLExecuteType(ACLExecuteType) {
+function getACLexecuteType(x) {
+  let executeACL = 'allow';
 
+  if (x === 1) {
+    executeACL = 'deny';
+  } else if (x === 2) {
+    executeACL = 'gotons';
+  }
+
+  return executeACL
 }
 
-function getACLPerm(perm) {
-// TODO: ACL 권한 인덱스값 
+function getACLPerm(x) {
+  let output = '';
+
+  return output
 }
 
 export const ACLFunction = {
-  getACLType,
-  getACLExecuteType,
+  getACLtype,
+  getACLexecuteType,
   getACLPerm
 };
