e631437c16bbfb356b1dbd8ebb73d023721e74b2
theseeduse <qwert304411@naver.com>
2025-02-11 17:16:32 +0900

    이걸 떼도 문제가 없네

--- utils/type.js
+++ utils/type.js
@@ -1,30 +1,30 @@
 export const types = {
     UserType: {
-        "delete": 0,
-        "ip": 1,
-        "author": 2
+        delete: 0,
+        ip: 1,
+        author: 2
     },
     ACLType: {
-        "perm": 0,
-        "user": 1,
-        "geoip": 2,
-        "aclgroup": 3,
-        "ip": 4
+        perm: 0,
+        user: 1,
+        geoip: 2,
+        aclgroup: 3,
+        ip: 4
     },
     ACLExecuteType: {
-        "allow": 0,
-        "deny": 1,
-        "gotons": 2,
+        allow: 0,
+        deny: 1,
+        gotons: 2,
     },
     ACLPermType: {
-        "any": 0,
-        "author": 1,
-        "signup_15day_later": 2,
-        "docname_same_username": 3,
-        "auto_verfied": 4,
-        "document_contributor": 5,
-        "contributor": 6,
-        "admin": 7,
-        "ip": 8
+        any: 0,
+        author: 1,
+        signup_15day_later: 2,
+        docname_same_username: 3,
+        auto_verfied: 4,
+        document_contributor: 5,
+        contributor: 6,
+        admin: 7,
+        ip: 8
     }
 };
