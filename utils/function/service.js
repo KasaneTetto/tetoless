36551ae64588dcaed49fc14cabd6a008ac9d98e3
theseeduse <qwert304411@naver.com>
2025-02-14 17:12:22 +0900

    export 개선

--- utils/function/service.js
+++ utils/function/service.js
@@ -1,9 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pgInit = {
+    "user": process.env.DB_USERID,
+    "host": process.env.DB_HOST,
+    "database": process.env.DB_NAME,
+    "password": process.env.DB_PASSWD,
+    "port": process.env.DB_PORT
+}
+
 export default function service() {
-    const pgInit = {
-        "user": process.env.DB_USERID,
-        "host": process.env.DB_HOST,
-        "database": process.env.DB_NAME,
-        "password": process.env.DB_PASSWD,
-        "port": process.env.DB_PORT
-    }
+    pgInit
 }
